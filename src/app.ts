59ef90119afa54337e47b2254cc9e63246524188
lqd1434 <1434288209@qq.com>
2022-03-13 23:29:46 +0800

    update CI/CD command

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from 'cors'
 import * as path from 'path'
 const app = express()
 import router from './router/update'
+import fileUpload from 'express-fileupload'
 
 app.use('/assets', express.static(path.resolve(__dirname, '../public')))
 app.use(
@@ -13,6 +14,7 @@ app.use(
 	bodyParser.urlencoded({ extended: true }),
 )
 app.use(cors())
+app.use(fileUpload({}))
 
 app.use('/update', router)
 
--- src/router/update.ts
+++ src/router/update.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import path from 'path'
+import fs from 'fs'
 const router = express.Router()
 
 // middleware that is specific to this router
@@ -10,6 +12,26 @@ router.use(function timeLog(req, res, next) {
 // define the home page route
 router.get('/check', function (req, res) {
 	res.send('Birds home page')
+})
+
+router.post('/uploadWin', (req: any, res) => {
+	try {
+		const { name, data } = req.files.file
+		const filepath = path.resolve(process.cwd(), './public/zip/win' + name)
+		fs.writeFile(filepath, data, () => {
+			console.log('文件写入成功')
+			res.send({
+				status: 200,
+				data: '文件上传成功',
+			})
+		})
+	} catch (error) {
+		console.log(error)
+		res.send({
+			status: 500,
+			data: '文件写入失败',
+		})
+	}
 })
 
 export default router
