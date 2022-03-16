e52b5eaa1a8384895d0f8e7f2865219e8f6c85b8
lqd1434 <1434288209@qq.com>
2022-03-16 15:16:57 +0800

    update code

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express()
 import router from './router/update'
 import fileUpload from 'express-fileupload'
 
-app.use('/assets', express.static(path.resolve(__dirname, '../public')))
+app.use('/assets', express.static(path.resolve(process.cwd(), '../update_asserts/')))
 app.use(
 	bodyParser.json(),
 	bodyParser.text(),
